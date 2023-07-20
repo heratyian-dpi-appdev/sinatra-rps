4b292e991aa9dd160358db637699da3e9d857aae
Ian Heraty <heratyian@gmail.com>
2023-07-20 17:13:53 +0000

    Finish rps tests

--- app.rb
+++ app.rb
@@ -2,8 +2,31 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:root)
+end
+
+get("/:move") do
+  @user_move = params[:move].downcase
+  @computer_move = ["rock", "paper", "scissors"].sample
+  @outcome = calculate_outcome(@user_move, @computer_move)
+
+  erb :move
+end
+
+def calculate_outcome(user_move, computer_move)
+  if user_move == "rock"
+    return "won" if computer_move == "scissors"
+    return "lost" if computer_move == "paper"
+    return "tied" if computer_move == "rock"
+  elsif user_move == "paper"
+    return "won" if computer_move == "rock"
+    return "lost" if computer_move == "scissors"
+    return "tied" if computer_move == "paper"
+  elsif user_move == "scissors"
+    return "won" if computer_move == "paper"
+    return "lost" if computer_move == "rock"
+    return "tied" if computer_move == "scissors"
+  else
+    return "illegal move"
+  end
 end
